29e421a6883bfaca399067bec488ffca33acddfe
Sviatoslava13 <sviatoslavamar@gmail.com>
2022-09-24 00:58:51 +0300

    exercise final 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -4,36 +4,29 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const inputEl = document.querySelector('#controls input');
-const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]');
-const divContainer = document.querySelector('#boxes');
-
+const divRef = document.querySelector("#boxes");
+const btnCreateRef = document.querySelector("[data-create]");
+const btnDestroyRef = document.querySelector("[data-destroy]");
+const inputRef = document.querySelector("input");
 let size = 30;
 
-const createBoxes = amount => {
-  //console.log('hi', amount);
-
-  const newDivContainer = document.createElement('div');
-  newDivContainer.style.background = getRandomHexColor();
-  newDivContainer.style.height = size + 'px';
-  newDivContainer.style.width = size + 'px';
-  newDivContainer.style.margin = '10px';
-  newDivContainer.classList.add('box');
-  divContainer.append(newDivContainer);
-  console.log(newDivContainer);
-  size += 10;
-};
-
-createBtn.addEventListener('click', createBoxes);
-
-const destroyBoxes = () => {
-  const boxDivEl = document.querySelectorAll('.box');
-
-  for (let boxDiv of boxDivEl) {
-    divContainer.removeChild(boxDiv);
+let elements = [];
+function createBoxes(amount) {
+  for (let i = 0; i <amount; i += 1) {
+     const div = document.createElement("div");
+  div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    elements.push(div);
   }
+  divRef.append(...elements);
+}
+btnCreateRef.addEventListener('click', (event) => {
+  createBoxes(inputRef.value)
+});
+btnDestroyRef.addEventListener('click', (event) => {
+  divRef.innerHTML = "";
+  elements = [];
   size = 30;
-};
-
-destroyBtn.addEventListener('click', destroyBoxes);
+});
